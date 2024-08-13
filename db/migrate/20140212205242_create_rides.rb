05f2071d2b3e68294901fc2daa8b57a025beadf4
Liam Simmons <liam.simmons@powerhrg.com>
2024-08-13 14:32:16 -0400

    Model relationships

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,9 @@
 class CreateRides < ActiveRecord::Migration[5.0]
   def change
     create_table :rides do |t|
+      t.references :passenger, foreign_key: true
+      t.references :taxi, foreign_key: true
+
       t.timestamps null: false
     end
   end
